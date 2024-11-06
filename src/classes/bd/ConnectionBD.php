493fa6a870b2080179731de8a212ead1ca25a346
AminoBela <abelaliabendjafar@gmail.com>
2024-11-06 17:32:32 +0100

    Premiers classes et structure du projet

---
+++ src/classes/bd/ConnectionBD.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace iutnc\nrv\bd;
+
+use PDO;
+
+class ConnectionBD
+{
+    private static array $tab = [];
+
+    public static ?PDO $pdo = null;
+
+    public static function setConfig(string $file): void
+    {
+        self::$tab = parse_ini_file($file);
+    }
+
+    public static function seConnecterBD(): ?PDO
+    {
+        if(is_null(self::$pdo)){
+            try {
+                $res = self::$tab['driver'] . ':host=' . self::$tab['host'] . ';dbname=' . self::$tab['dbname'];
+                self::$pdo = new PDO($res, self::$tab['user'], self::$tab['password']);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo 'Connexion échouée à la base de données : ' . $e->getMessage();
+            }
+        }
+        return self::$pdo;
+    }
+
+}
---
+++ src/classes/renderer/Renderer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace iutnc\nrv\renderer;
+
+class Renderer
+{
+
+}
