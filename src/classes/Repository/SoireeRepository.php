c3404d9a1a25e6469062715e687053a08887348d
ValentinoLambert <160715924+ValentinoLambert@users.noreply.github.com>
2024-11-07 08:58:55 +0100

    Ajout des repo

---
+++ src/classes/Repository/SoireeRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace iutnc\nrv\repository;
+
+use iutnc\nrv\bd\ConnectionBD;
+use PDO;
+
+class SoireeRepository {
+    private PDO $db;
+
+    public function __construct() {
+        $this->db = ConnectionBD::seConnecterBD();
+    }
+
+    public function ajouterSoiree($nom, $thematique, $date, $horaireDebut, $idLieu) {
+        $query = "INSERT INTO Soirees (nom, thematique, date, horaireDebut, idLieu) VALUES (:nom, :thematique, :date, :horaireDebut, :idLieu)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(":nom", $nom);
+        $stmt->bindParam(":thematique", $thematique);
+        $stmt->bindParam(":date", $date);
+        $stmt->bindParam(":horaireDebut", $horaireDebut);
+        $stmt->bindParam(":idLieu", $idLieu);
+        return $stmt->execute();
+    }
+
+    public function obtenirSoireeParId($idSoiree) {
+        $query = "SELECT * FROM Soirees WHERE idSoiree = :idSoiree";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(":idSoiree", $idSoiree);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
