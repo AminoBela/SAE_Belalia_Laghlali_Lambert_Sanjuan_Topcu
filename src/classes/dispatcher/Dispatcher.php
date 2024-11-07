a91a526c0f3659de46801362c2ff29d3c0a4f295
AminoBela <abelaliabendjafar@gmail.com>
2024-11-07 08:16:40 +0100

    Ajout du config.ini, index.php et travail sur Dispatcher

--- src/classes/dispatcher/Dispatcher.php
+++ src/classes/dispatcher/Dispatcher.php
@@ -5,4 +5,21 @@ namespace iutnc\nrv\dispatcher;
 class Dispatcher
 {
 
+    private string $action;
+
+    public function __construct() {
+        $this->action = "";
+        if (isset($_GET['action'])) $this->action = $_GET['action'];
+    }
+
+    public function run() {
+        switch ($this->action) {
+            default:
+                echo "default";
+                break;
+        }
+    }
+
+
+
 }
