64f70e334c1ea31eef2888ffee466e6b0d54ad65
ValentinoLambert <160715924+ValentinoLambert@users.noreply.github.com>
2024-11-08 11:17:56 +0100

    Rajout fonctionnalité 1

--- src/classes/repository/SpectacleRepository.php
+++ src/classes/repository/SpectacleRepository.php
@@ -1,19 +1,28 @@
 <?php
-
 namespace iutnc\nrv\repository;
 
-use PDO;
 use iutnc\nrv\bd\ConnectionBD;
+use PDO;
 
-class SpectacleRepository
-{
-
+class SpectacleRepository {
     private PDO $pdo;
 
     public function __construct() {
-        $this->pdo = ConnectionBD::obtenirBD();
+        $this->pdo = ConnectionBD::getInstance()->getConnection();
     }
 
-
-
+    public function getListeSpectacles(): array {
+        $query = "
+            SELECT s.titre, s.description, s.horrairePrevuSpectacle, so.dateSoiree, i.urlImage
+            FROM Spectacle s
+            LEFT JOIN SoireeToSpectacle sts ON s.idSpectacle = sts.idSpectacle
+            LEFT JOIN Soiree so ON sts.idLieu = so.idLieu AND sts.dateSoiree = so.dateSoiree
+            LEFT JOIN ImageToSpectacle its ON s.idSpectacle = its.idSpectacle
+            LEFT JOIN Image i ON its.idImage = i.idImage;
+        ";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
+?>
